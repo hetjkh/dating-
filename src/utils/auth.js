65c61e6eaf682c273b818cbb0123f50d3b2cb3df
het <hetj8388@gmail.com>
2025-04-14 11:46:55 +0530

    new urleeee

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API = "https://dating-backend.onrender.com";
+const API = "https://dating-backend-1h4q.onrender.com";
 
 export const register = async (name, email, password) => {
   return axios.post(`${API}/register`, { name, email, password }, { withCredentials: true });
